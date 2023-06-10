73c9b4ff277c2cd364e9aa99b09e7a86078855f8
Esther Dhiramo <133733630+EstherDhiramo@users.noreply.github.com>
2023-06-09 21:56:02 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,243 @@
+"use strict";
+
+const keyboardContainer = document.querySelector(".keyboard");
+let options = [];
+
+let selectedOption = null;
+let SVG = null;
+
+const features = {
+  case: {
+    metalCase: false,
+    plasticCase: true,
+  },
+  cable: {
+    coiledCable: true,
+    straightCable: false,
+    wireless: false,
+  },
+  wristRest: {
+    wristRest: false,
+  },
+};
+const common = [
+  "#ef476f",
+  "#ffd166",
+  "#06d6a0",
+  "#118ab2",
+  "#073b4c",
+  "ghostwhite",
+];
+const colors = {
+  plasticCase: common,
+  coiledCable: common,
+  metalCase: common,
+  wristRest: ["#b37447", "#f4bc7c", "#ffd5a5"],
+  keycapsMid: common,
+  keycapsRight: common,
+  keycapsLeft: common,
+};
+
+async function init() {
+  const res = await fetch("./images/mainSvg/KeyboardMod.svg");
+  const svgText = await res.text();
+  keyboardContainer.innerHTML += svgText;
+
+  const svg = document.querySelector("#keyboard");
+  SVG = svg;
+
+  const groups = svg.querySelectorAll("g:not(.shadow, .keycaps)");
+
+  groups.forEach((g) => {
+    g.addEventListener("click", () => {
+      const clickedFeature = g.id;
+
+      const optionItems = document.querySelectorAll(".option-item");
+      optionItems.forEach((item) => {
+        item.classList.remove("focused");
+      });
+
+      groups.forEach((g) => {
+        g.classList.remove("focused");
+      });
+
+      g.classList.add("focused");
+
+      const activeOptionItem = document.querySelector(
+        `.option-item[data-feature=${clickedFeature}]`
+      );
+
+      if (activeOptionItem) {
+        activeOptionItem.classList.add("focused");
+      }
+
+      selectedOption = clickedFeature;
+
+      showColors(clickedFeature);
+    });
+  });
+
+  const optionItems = document.querySelectorAll(".option-item");
+  optionItems.forEach((item) => {
+    const category = item.dataset.category;
+    const feature = item.dataset.feature;
+    item.addEventListener("click", (e) => {
+      updateItemState(category, feature);
+      selectedOption = feature;
+
+      if (features[category][feature]) {
+        item.classList.add("focused", "active");
+      } else {
+        item.classList.remove("active");
+      }
+
+      if (
+        !document.querySelector(`.selectedFeature[data-feature=${feature}]`) &&
+        features[category][feature]
+      ) {
+        addToSelected(e.target, feature, category);
+      }
+    });
+  });
+
+  updateSVGState();
+}
+
+function addToSelected(target, feature, category) {
+  const selectedFeature = createFeatureElement(feature, category);
+  document.querySelector(".selectedFeatures").appendChild(selectedFeature);
+
+  const startPos = target.getBoundingClientRect();
+  console.log(startPos);
+
+  const endPos = selectedFeature.getBoundingClientRect();
+
+  const difX =
+    startPos.left - endPos.left + startPos.width / 2 - endPos.width / 2;
+  const difY =
+    startPos.top - endPos.top + startPos.height / 2 - endPos.height / 2;
+
+  selectedFeature.style.transform = `translate(${difX}px, ${difY}px)`;
+
+  requestAnimationFrame(() => {
+    selectedFeature.style.transition = `all 0.5s linear`;
+    selectedFeature.style.transform = `translate(0, 0)`;
+  });
+}
+
+function createFeatureElement(feature, category) {
+  const div = document.createElement("div");
+  div.classList.add("selectedFeature");
+  div.dataset.feature = feature;
+
+  div.dataset.category = category;
+
+  const img = document.createElement("img");
+  img.src = `./images/features/${feature}.svg`;
+  img.alt = feature;
+
+  div.append(img);
+
+  return div;
+}
+
+function updateItemState(category, feature) {
+  const optionItems = document.querySelectorAll(".option-item");
+  optionItems.forEach((item) => {
+    item.classList.remove("focused");
+    if (item.dataset.category === category) {
+      item.classList.remove("active");
+
+      if (
+        document.querySelectorAll(`.selectedFeature[data-category=${category}]`)
+          .length > 0
+      ) {
+        document
+          .querySelectorAll(`.selectedFeature[data-category=${category}]`)
+          .forEach((item) => {
+            item.remove();
+          });
+      }
+    }
+  });
+
+  const featuresInCategory = Object.keys(features[category]);
+  featuresInCategory.forEach((featureKey) => {
+    if (featureKey !== "wristRest") features[category][featureKey] = false;
+  });
+
+  features[category][feature] = !features[category][feature];
+
+  if (features[category][feature]) {
+    showColors(feature);
+  } else {
+    const colorPicker = document.querySelector(".color-picker");
+    colorPicker.innerHTML = "";
+  }
+
+  updateSVGState();
+}
+
+function updateSVGState() {
+  const allCategoryKeys = Object.keys(features);
+
+  allCategoryKeys.forEach((categoryKey) => {
+    const allFeatureKeys = Object.keys(features[categoryKey]);
+    allFeatureKeys.forEach((featureKey) => {
+      const svgElement = SVG.querySelector(`#${featureKey}`);
+      if (svgElement) {
+        if (features[categoryKey][featureKey]) {
+          svgElement.style.display = "block";
+        } else {
+          svgElement.style.display = "none";
+        }
+      }
+    });
+  });
+}
+
+function showColors(feature) {
+  const colorPicker = document.querySelector(".color-picker");
+  runAnimationOnce(colorPicker, "jumpout");
+  colorPicker.innerHTML = "";
+
+  if (!colors[feature]) return;
+
+  colors[feature].forEach((colorCode) => {
+    const colorElement = document.createElement("div");
+    colorElement.classList.add("color");
+    colorElement.style.setProperty("--color", colorCode);
+    colorElement.dataset.value = colorCode;
+    colorPicker.appendChild(colorElement);
+  });
+
+  colorPicker.querySelectorAll(".color").forEach((color) => {
+    color.addEventListener("click", () => {
+      colorPicker.querySelectorAll(".color").forEach((color) => {
+        color.classList.remove("active");
+      });
+      color.classList.add("active");
+      SVG.querySelector(`#${selectedOption}`).style.color = color.dataset.value;
+    });
+  });
+}
+
+function runAnimationOnce(element, className, callback = () => {}) {
+  if (!element) return;
+  element.classList.add(className);
+  element.addEventListener("animationend", () => {
+    element.classList.remove(className);
+    callback();
+  });
+}
+
+init();
+gsap.registerPlugin(ScrollTrigger);
+document.querySelectorAll("[data-scrollTrigger]").forEach((el) => {
+  console.log(el, el.dataset.duration);
+  gsap.from(el, {
+    y: 100,
+    duration: parseInt(el.dataset.duration) || 2,
+    scrollTrigger: el,
+  });
+});
---
+++ scroll.js
@@ -0,0 +1,101 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", animateText);
+
+////////////////////   Animate text like Peter's coolText animation ///////////////
+function animateText() {
+  const coolText = document.querySelector("#cooltext");
+  let letterByLetter = Array.from(coolText.textContent);
+  console.log(letterByLetter);
+  coolText.innerHTML = "";
+  letterByLetter.forEach((letter, index) => {
+    const animateLetter = document.createElement("span");
+    animateLetter.classList.add("letter", "fade-in-bottom");
+    animateLetter.style.setProperty("--letter", index);
+    if (letter === " ") {
+      animateLetter.innerHTML = "&nbsp;";
+    } else {
+      animateLetter.textContent = letter;
+    }
+    coolText.append(animateLetter);
+  });
+}
+
+////////////// Text easing in from bottom /////////////
+gsap.registerPlugin(ScrollTrigger);
+document.querySelectorAll("[data-scrollTrigger]").forEach((el) => {
+  console.log(el, el.dataset.duration);
+  gsap.from(el, {
+    y: 100,
+    duration: parseInt(el.dataset.duration) || 2,
+    scrollTrigger: el,
+  });
+});
+
+/* /////////// Additional features horizontal scroll/swipe /////////
+const cards = document.querySelectorAll(".card");
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function isCardVisible() {
+  cards.forEach((card) => {
+    isElementInViewport(card)
+      /* ? card.classList.add("isVisible")
+      : card.classList.remove("isVisible");
+  });
+}
+
+document.addEventListener("DOMContentLoaded", isCardVisible);
+window.addEventListener("scroll", isCardVisible);
+window.addEventListener("resize", isCardVisible); */
+
+/////////// Parallax /////////
+gsap.to(".slower", {
+  y: 800,
+  rotate: -150,
+  scale: 0.1,
+  ease: "none",
+  scrollTrigger: {
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true,
+  },
+});
+gsap.to(".faster", {
+  y: -1800,
+  rotate: 160,
+  scale: 3,
+  ease: "none",
+  scrollTrigger: {
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true,
+  },
+});
+
+gsap.from("#section-three img", {
+  y: -200,
+  ease: "ease-in-out",
+  scrollTrigger: {
+    trigger: "#section-three .section-desc",
+  },
+});
+
+gsap.from(".feature", {
+  y: 100,
+  opacity: 0,
+  stagger: 0.3,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".additional-features",
+  },
+});
